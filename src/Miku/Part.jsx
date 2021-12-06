0cacd253cc86dfd53333946bf249098afd09e9ae
yuyuyzl <yuzhongliang221@gmail.com>
2021-12-06 12:47:34 +0800

    fix scale Y

--- src/Miku/Part.jsx
+++ src/Miku/Part.jsx
@@ -5,9 +5,10 @@ export default function Part({config}){
         position:'absolute',
         transform:`translate(${x}px,${y}px) `+(rotation?`rotate(${rotation}deg)`:''),
     }}>
+        {/*<div>{id}</div>*/}
         <img src={resource} style={{
             position:'absolute',
-            transform:`translate(${-resourceCenterX}px,${-resourceCenterY}px) scaleY(${scaleY})`,
+            transform:`translate(${-resourceCenterX}px,${-resourceCenterY}px)`+(scaleY?` scaleY(${scaleY})`:''),
         }}/>
         {components?.map((o,i)=>
             <Part config={o} key={i}/>
