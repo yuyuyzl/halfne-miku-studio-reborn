109308314c2661b43408efddd4c0335c500be468
yuyuyzl <yuzhongliang221@gmail.com>
2021-12-12 05:09:54 +0800

    remove safe eval

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import safeEval from 'safe-eval-2';
 
 ReactDOM.render(
   <React.StrictMode>
