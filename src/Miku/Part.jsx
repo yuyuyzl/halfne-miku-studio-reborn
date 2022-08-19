9cd469ae00dcccafbb465deb4213a03ba44a2626
yuyuyzl <yuzhongliang221@gmail.com>
2022-08-20 02:58:27 +0800

    background

--- src/Miku/Part.jsx
+++ src/Miku/Part.jsx
@@ -1,14 +1,14 @@
 export default function Part({renderState}){
     if (!renderState)return null;
-    const {id,x,y,components,resource,resourceCenterX,resourceCenterY,rotation,virtual,scaleY}=renderState;
+    const {id,x,y,components,resource,resourceCenterX,resourceCenterY,rotation,virtual,scaleY,scaleX}=renderState;
     if(virtual)return null;
     return <div data-id={id} style={{
         position:'absolute',
-        transform:`translate(${x}px,${y}px) `+(rotation?`rotate(${rotation}deg)`:''),
+        transform:`translate(${x}px,${y}px) `+(rotation?`rotate(${rotation}deg)`:'')+(scaleY?` scaleY(${scaleY})`:'')+(scaleX?` scaleX(${scaleX})`:''),
     }}>
         <img src={resource} style={{
             position:'absolute',
-            transform:`translate(${-resourceCenterX}px,${-resourceCenterY}px)`+(scaleY?` scaleY(${scaleY})`:''),
+            transform:`translate(${-resourceCenterX}px,${-resourceCenterY}px)`,
         }}/>
         {components?.map((o,i)=>
             <Part renderState={o} key={i}/>
