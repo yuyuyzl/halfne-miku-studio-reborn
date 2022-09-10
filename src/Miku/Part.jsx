364803dc56b4917f2e363b2fcd3ed482d06c1515
yuyuyzl <yuzhongliang221@gmail.com>
2022-09-11 04:20:32 +0800

    skew

--- src/Miku/Part.jsx
+++ src/Miku/Part.jsx
@@ -1,10 +1,17 @@
 export default function Part({renderState}){
     if (!renderState)return null;
-    const {id,x,y,components,resource,resourceCenterX,resourceCenterY,rotation,virtual,scaleY,scaleX}=renderState;
+    const {id,x,y,components,resource,resourceCenterX,resourceCenterY,rotation,virtual,scaleY,scaleX,skewX,skewY}=renderState;
     if(virtual)return null;
     return <div data-id={id} style={{
         position:'absolute',
-        transform:`translate(${x}px,${y}px) `+(rotation?`rotate(${rotation}deg)`:'')+(scaleY?` scaleY(${scaleY})`:'')+(scaleX?` scaleX(${scaleX})`:''),
+        transform:
+            (skewX?` skewX(${-skewX}deg)`:'')+
+            (skewY?` skewY(${skewY}deg)`:'')+
+            `translate(${x}px,${y}px) `+
+            (rotation?`rotate(${rotation}deg)`:'')+
+            (scaleY?` scaleY(${scaleY})`:'')+
+            (scaleX?` scaleX(${scaleX})`:'')
+        ,
     }}>
         {resource?<img src={resource} style={{
             position: 'absolute',
